8a3accd2ef0fcd18f59dfa9c60bad2c0feade6e8
AmirMohamad1796 <amirmohammad.momenipor@gmail.com>
2024-10-30 14:50:12 +0330

    Update Code

--- app/src/main/java/com/amirmohammad/test2_digi/CartManager.kt
+++ app/src/main/java/com/amirmohammad/test2_digi/CartManager.kt
@@ -1,37 +1,3 @@
-//package com.amirmohammad.test2_digi
-//
-//import android.content.Context
-//import com.google.gson.Gson
-//import com.google.gson.reflect.TypeToken
-//
-//object CartManager {
-//
-//    private const val PREFS_NAME = "cart_prefs"
-//    private const val CART_ITEMS_KEY = "cart_items"
-//
-//    fun saveCart(context: Context, products: List<Product>) {
-//        val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
-//        val editor = prefs.edit()
-//        val json = Gson().toJson(products)
-//        editor.putString(CART_ITEMS_KEY, json)
-//        editor.apply()
-//    }
-//
-//    fun loadCart(context: Context): List<Product> {
-//        val prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
-//        val json = prefs.getString(CART_ITEMS_KEY, null)
-//        return if (json != null) {
-//            val type = object : TypeToken<List<Product>>() {}.type
-//            Gson().fromJson(json, type)
-//        } else {
-//            emptyList()
-//        }
-//    }
-//}
-
-
-//*************
-
 package com.amirmohammad.test2_digi
 
 import android.content.Context
